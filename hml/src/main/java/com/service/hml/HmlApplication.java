82fcefa5889eade35d170a368c29b6968d20fe42
Miguel <mbeiraob@gmail.com>
2022-05-31 11:28:46 +0100

    Service API setup completed

---
+++ hml/src/main/java/com/service/hml/HmlApplication.java
@@ -0,0 +1,13 @@
+package com.service.hml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmlApplication.class, args);
+	}
+
+}
---
+++ hml/src/test/java/com/service/hml/HmlRestApiTests.java
@@ -0,0 +1,105 @@
+package com.service.hml;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK, classes = HmlApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class HmlRestApiTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private HmlRepository repository;
+
+    @AfterEach
+    public void resetDb() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void whenValidInput_thenCreateBook() throws IOException, Exception{
+        Book ford = new Book(1, "Ford", "2014 Tauros", "");
+
+        mvc.perform(post("/hml/api/addBook")
+                        .content(asJsonString(ford))
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isCreated());
+
+        List<Book> found = repository.findAll();
+        assertThat(found).extracting(Book::getTitle).containsOnly("Ford");
+    }
+
+    @Test
+    void whenGetBookAllBooks_thenGetAllBooks() throws Exception {
+        Book ford = new Book(1, "Ford", "2014 Tauros", "");
+        Book audi = new Book(2, "Audi", "Audi A8", "");
+        Book bmw = new Book(3, "BMW", "BMW M4", "");
+
+        repository.saveAndFlush(ford);
+        repository.saveAndFlush(audi);
+        repository.saveAndFlush(bmw);
+
+        mvc.perform(get("/hml/api/allBooks").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(3))));
+    }
+
+    @Test
+    void whenValidTittle_thenGetBook() throws IOException, Exception{
+        Book ford = new Book(4, "Book", "2014 Tauros", "");
+        repository.saveAndFlush(ford);
+
+        System.out.println(repository.findAll());
+        mvc.perform(get("/hml/api/book/"+ford.getTitle()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    void whenInvalidTittle_thenGetNotFound() throws IOException, Exception{
+        mvc.perform(get("/hml/api/book/title").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenValidInput_thenCreateDelivery() throws IOException, Exception{
+        Book ford = new Book(1, "Ford", "2014 Tauros", "");
+        //mvc.perform(post("/hml/api/delivery").contentType(MediaType.APPLICATION_JSON).content(JsonUtils.toJson(ford)));
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
